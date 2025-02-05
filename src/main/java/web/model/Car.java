5d1a9371bc5df4fcc391ecaa748b7c1b04a82b81
admin <gablin233@mail.ru>
2025-02-05 17:24:22 +0300

    Переделал согласно указаниям

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,6 +1,21 @@
 package web.model;
 
+import java.util.Objects;
+
 public class Car {
+    String model;
+    String color;
+    int year;
+
+    public Car() {
+    }
+
+    public Car(int year, String color, String model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+
     public String getModel() {
         return model;
     }
@@ -21,9 +36,7 @@ public class Car {
         return year;
     }
 
-    public Car(String model, String color, int year) {
-        this.model = model;
-        this.color = color;
+    public void setYear(int year) {
         this.year = year;
     }
 
@@ -36,20 +49,15 @@ public class Car {
                 '}';
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(model, car.model) && Objects.equals(color, car.color);
     }
 
-    public Car() {
-    }
-
-    String model;
-    String color;
-    int year;
-
-    public Car(int year, String color, String model) {
-        this.year = year;
-        this.color = color;
-        this.model = model;
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, color, year);
     }
 }
